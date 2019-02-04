e32fba00b50da253daa78c5224775ba6fb2794a4
Bill Shoener <nitrocipher@gmail.com>
2019-02-04 09:57:30 -0600

    set document.title

--- ext2to3.user.js
+++ ext2to3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scratch ext2to3
 // @namespace    https://github.com/NitroCipher/ext2to3
-// @version      0.6g
+// @version      0.6h
 // @description  try to take over the world!
 // @author       NitroCipher and Jamesbmadden
 // @match        https://scratch.mit.edu/convert/*
@@ -19,6 +19,7 @@
     var argDescriptor;
     var argDefaults;
     var argNames;
+    document.title = "Ext2to3 - Convert 2.0 extensions to work with 3.0";
     $(".box-head").html("<h2>Ext2to3</h2><button id='extDownload' type='button'>Please wait as we make the 3.0 extension</button>"); // While it's loading, show some content
     $(".box-content").html(`<img src="//cdn.scratch.mit.edu/scratchr2/static/__238e692fba4864e9ff337c17d8a4033d__/images/404-giga.png" style="margin:20px;padding: 10px;"><div class="box-header"> <h1>Trying to convert ${url}...</h1></div><br><p>If this message doesn't disappear, than this extension may be incompatible at this time, please check back later, or <a href="https://github.com/NitroCipher/ext2to3/issues/new">create and issue on our github</a></p><br><br>`);
     $.ajax({
